9ba8080016a712fe09115297358ca88210c8a69a
OReilly19 <daveor19@hotmail.com>
2015-03-06 15:47:43 +0000

    V2

---
+++ DCUClubsAndSocsV2/app/src/main/java/app/AppConfig.java
@@ -0,0 +1,9 @@
+package app;
+
+public class AppConfig {
+    // Server user login url
+    public static String URL_LOGIN = "http://136.206.95.86/android_login_api/";
+
+    // Server user register url
+    public static String URL_REGISTER = "http://136.206.95.86/android_login_api/";
+}
